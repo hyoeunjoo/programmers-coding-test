61cd70ff110f69edf44eafa91aac8a4076a396f2
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-11-03 23:49:59 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,25 +1,6 @@
 class Solution {
-    public int solution(int[] numbers) {
-        int lcmAnswer = numbers[0];  
-
-        for (int i = 1; i < numbers.length; i++) {
-            lcmAnswer = calculateLCM(lcmAnswer, numbers[i]);
-        }
-
-        return lcmAnswer;
-    }
-
-    private int calculateLCM(int a, int b) {
-        int gcd = calculateGCD(a, b);  
-        return (a / gcd) * b;  
-    }
-
-    private int calculateGCD(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+    public int solution(int[] arr) {
+        int answer = 0;
+        return answer;
     }
 }
