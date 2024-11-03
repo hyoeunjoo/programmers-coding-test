5a5a2f3896c2834bba1b612bc0df56b5dbc12b3b
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-11-03 23:37:58 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int n) {
-        String basedTernary = new StringBuilder(Integer.toString(n, 3)).reverse().toString();
-        int answer = Integer.parseInt(basedTernary, 3);
-        
+        int answer = 0;
         return answer;
     }
 }
