0f8e461a4e497ffea885a6a038e36ad058aca290
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2025-11-25 02:08:59 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 0.05 ms
+메모리: 75.6 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 13:43:57
+2025년 11월 25일 02:08:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int sumOfCells = brown + yellow;
-
-        for (int h = 3; h * h <= sumOfCells; h++) {
-            if (sumOfCells % h == 0) {
-                int w = sumOfCells / h;
-
-                if ((w - 2) * (h - 2) == yellow) {
-                    return new int[]{w, h};
-                }
+        
+        int [] answer = new int[2]; 
+        for (int i = 1 ; i <= yellow ; i++){
+            
+            if(yellow % i == 0){
+                int width = yellow/i;
+                int height = i;
+                
+                if(width >= height){
+                    int total = (width + 2) * (height + 2) - yellow;
+                    
+                    if(total == brown){
+                        answer[0] = width+2;
+                        answer[1] = height+2;
+                        return answer;
+                    }
+                }else break;
             }
+            
         }
-        return new int[0];
-    }
+        return answer; 
+}
 }
